645cb89148b51bdd17fdac5b77e6b834a50fe669
derekatharbor <hello@useharbor.io>
2025-11-23 22:18:08 -0600

    new hero

---
+++ apps/web/app/preview/page.tsx
@@ -0,0 +1,11 @@
+import FrostedNav from '@/components/landing/FrostedNav'
+import NewHeroSection from '@/components/landing/NewHeroSection'
+
+export default function PreviewPage() {
+  return (
+    <div className="relative min-h-screen bg-[#101A31]">
+      <FrostedNav />
+      <NewHeroSection />
+    </div>
+  )
+}
