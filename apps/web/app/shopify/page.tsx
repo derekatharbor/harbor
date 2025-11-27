f339eb474ff676b4f9322d3d5fc2009849dbeef9
derekatharbor <hello@useharbor.io>
2025-11-27 12:51:57 -0600

    shopify imports

--- apps/web/app/shopify/page.tsx
+++ apps/web/app/shopify/page.tsx
@@ -3,25 +3,25 @@
 
 import ShopifyNav from '@/components/shopify/ShopifyNav'
 import ShopifyHero from '@/components/shopify/ShopifyHero'
-import ShopifyShift from '@/components/shopify/ShopifyShift'
-import ShopifyProblem from '@/components/shopify/ShopifyProblem'
-import ShopifyCapabilities from '@/components/shopify/ShopifyCapabilities'
-import ShopifyCTA from '@/components/shopify/ShopifyCTA'
+// import ShopifyShift from '@/components/shopify/ShopifyShift'
+// import ShopifyProblem from '@/components/shopify/ShopifyProblem'
+// import ShopifyCapabilities from '@/components/shopify/ShopifyCapabilities'
+// import ShopifyCTA from '@/components/shopify/ShopifyCTA'
 import ShopifyFooter from '@/components/shopify/ShopifyFooter'
 import { ShopifyWaitlistProvider } from '@/components/shopify/useShopifyWaitlist'
 
 export default function ShopifyWaitlistPage() {
   return (
     <ShopifyWaitlistProvider>
-      <div className="min-h-screen bg-[#0a0f1a]">
+      <div className="min-h-screen bg-[#101A31]">
         <ShopifyNav />
         
         <main>
           <ShopifyHero />
-          <ShopifyShift />
-          <ShopifyProblem />
-          <ShopifyCapabilities />
-          <ShopifyCTA />
+          {/* <ShopifyShift /> */}
+          {/* <ShopifyProblem /> */}
+          {/* <ShopifyCapabilities /> */}
+          {/* <ShopifyCTA /> */}
         </main>
         
         <ShopifyFooter />
